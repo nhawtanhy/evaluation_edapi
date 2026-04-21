95c11a5c0fea25c18b8a1cb62524ca05a0534f0a
nhawtanhy <nhatanhvh11@gmail.com@users.noreply.github.com>
2026-04-21 15:50:28 +0700

    Run all

--- evaluate_base+RAG.py
+++ evaluate_base+RAG.py
@@ -105,7 +105,7 @@ def main():
     model.eval()
 
     # 3. PREPARE DATA
-    num_test_samples = 20 # Set to None to run all
+    num_test_samples = None # Set to None to run all
     with open(dataset_path, "r") as f:
         data = json.load(f)
     if num_test_samples:
